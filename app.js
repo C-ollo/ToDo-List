ff4f71e3b22fc83ea5224c1d1016ff1c1cf4cf3b
Collins Kimutai <collinskimutai72@gmail.com>
2023-05-01 11:34:14 +0300

    adding ejs

--- app.js
+++ app.js
@@ -4,9 +4,43 @@ const bodyParser = require('body-parser');
 const app= express();
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.set('view engine','ejs');
+
 
 app.get('/',function(req,res) {
-    res.send('Hello');
+    let today = new Date();
+
+    let currentDay = today.getDate();
+    let day ='';
+    switch (currentDay) {
+        case 0:
+          day = "Sunday";
+          break;
+        case 1:
+          day = "Monday";
+          break;
+        case 2:
+          day = "Tuesday";
+          break;
+        case 3:
+          day = "Wednesday";
+          break;
+        case 4:
+          day = "Thursday";
+          break;
+        case 5:
+          day = "Friday";
+          break;
+        case 6:
+          day = "Saturday";
+          break;
+        default:
+          day = "Invalid day of the week";
+          break;
+      }
+  
+    res.render('list',{day:day});
+
 });
 
 
