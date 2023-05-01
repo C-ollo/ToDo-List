df6d38572a357a990ded035b8a019af2c791f8b1
Collins Kimutai <collinskimutai72@gmail.com>
2023-05-01 12:45:47 +0300

    adding post

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.set('view engine','ejs');
 
-
+var items = [];
 app.get('/',function(req,res) {
     let options = {
         weekday: "long",
@@ -17,9 +17,16 @@ app.get('/',function(req,res) {
     let today = new Date();
 
     day = today.toLocaleDateString('en-US',options);
-    res.render('list',{day:day});
+    res.render('list',{day:day,newListItem:items});
 
 });
+
+app.post("/",function (req,res) {
+    item = req.body.newItem;
+    items.push(item);
+    res.redirect('/');
+    
+})
 
 
 app.listen(3000,function() {
