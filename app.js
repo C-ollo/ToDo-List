61d1f17e218ae812daaa11f8cd73ace1e59f15b4
Collins Kimutai <collinskimutai72@gmail.com>
2023-05-01 11:41:30 +0300

    using tolocalestring

--- app.js
+++ app.js
@@ -8,37 +8,15 @@ app.set('view engine','ejs');
 
 
 app.get('/',function(req,res) {
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
     let today = new Date();
 
-    let currentDay = today.getDate();
-    let day ='';
-    switch (currentDay) {
-        case 0:
-          day = "Sunday";
-          break;
-        case 1:
-          day = "Monday";
-          break;
-        case 2:
-          day = "Tuesday";
-          break;
-        case 3:
-          day = "Wednesday";
-          break;
-        case 4:
-          day = "Thursday";
-          break;
-        case 5:
-          day = "Friday";
-          break;
-        case 6:
-          day = "Saturday";
-          break;
-        default:
-          day = "Invalid day of the week";
-          break;
-      }
-  
+    day = today.toLocaleDateString('en-US',options);
     res.render('list',{day:day});
 
 });
